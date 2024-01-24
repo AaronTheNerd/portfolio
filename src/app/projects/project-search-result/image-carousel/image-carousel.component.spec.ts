37be6d2dc6571d870ed754290b128a41794f84b2
AaronTheNerd <bargeaaron@gmail.com>
2024-01-23 22:22:30 -0500

    Chore: Removed spec files

--- src/app/projects/project-search-result/image-carousel/image-carousel.component.spec.ts
+++ src/app/projects/project-search-result/image-carousel/image-carousel.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SidenavComponent } from './sidenav.component';
+import { ImageCarouselComponent } from './image-carousel.component';
 
-describe('SidenavComponent', () => {
-  let component: SidenavComponent;
-  let fixture: ComponentFixture<SidenavComponent>;
+describe('ImageCarouselComponent', () => {
+  let component: ImageCarouselComponent;
+  let fixture: ComponentFixture<ImageCarouselComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [SidenavComponent]
+      declarations: [ImageCarouselComponent]
     })
     .compileComponents();
     
-    fixture = TestBed.createComponent(SidenavComponent);
+    fixture = TestBed.createComponent(ImageCarouselComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
