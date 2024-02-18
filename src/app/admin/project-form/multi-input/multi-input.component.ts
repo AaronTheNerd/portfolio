8050b08caa193750aaea2cf02e11cdb41757b202
AaronTheNerd <bargeaaron@gmail.com>
2024-02-17 22:03:54 -0500

    Chore: Added multi-input component

---
+++ src/app/admin/project-form/multi-input/multi-input.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-input',
+  templateUrl: './multi-input.component.html',
+  styleUrl: './multi-input.component.scss'
+})
+export class MultiInputComponent {
+
+}
