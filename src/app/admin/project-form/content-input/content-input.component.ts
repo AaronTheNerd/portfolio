2e2b436b83141fd00cf2b03ec71c7caff12b9506
AaronTheNerd <bargeaaron@gmail.com>
2024-03-01 18:02:23 -0500

    Feature: Made it so that the project form can have different input types

--- src/app/admin/project-form/content-input/content-input.component.ts
+++ src/app/admin/project-form/content-input/content-input.component.ts
@@ -2,17 +2,24 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DynamicComponentEntry } from '../../../_models/project-document.model';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 
+interface ComponentFormEntry extends DynamicComponentEntry {
+  input_types?: { [key: string]: string }
+}
+
 @Component({
   selector: 'app-content-input',
   templateUrl: './content-input.component.html',
   styleUrl: './content-input.component.scss'
 })
 export class ContentInputComponent {
-  dynamicComponents: DynamicComponentEntry[] = [
+  dynamicComponents: ComponentFormEntry[] = [
     {
       componentType: "ParagraphComponent",
       inputs: {
         text: ""
+      },
+      input_types: {
+        text: "input"
       }
     },
     {
@@ -20,6 +27,10 @@ export class ContentInputComponent {
       inputs: {
         title: "",
         id: ""
+      },
+      input_types: {
+        title: "input",
+        id: "input"
       }
     },
     {
@@ -27,6 +38,10 @@ export class ContentInputComponent {
       inputs: {
         title: "",
         id: ""
+      },
+      input_types: {
+        title: "input",
+        id: "input"
       }
     },
     {
@@ -34,6 +49,23 @@ export class ContentInputComponent {
       inputs: {
         title: "",
         id: ""
+      },
+      input_types: {
+        title: "input",
+        id: "input"
+      }
+    },
+    {
+      componentType: "CodeFileComponent",
+      inputs: {
+        title: "",
+        content: "",
+        language: ""
+      },
+      input_types: {
+        title: "input",
+        content: "textarea",
+        language: "input"
       }
     },
   ];
@@ -71,11 +103,19 @@ export class ContentInputComponent {
     const default_entry = this.dynamicComponents[index];
     const entry = Object.assign({}, default_entry);
     entry.inputs = Object.assign({}, default_entry.inputs);
-    return entry;
+    delete entry.input_types;
+    return entry as DynamicComponentEntry;
   }
 
   set_input_value(index: number, key: string, value: string) {
     this.content[index].inputs[key] = value;
     this.contentChange.emit(this.content.slice());
+  }
+
+  get_input_types(componentType: string): {[key: string]: string} {
+    const entry = this.dynamicComponents.find((entry) => {
+      return entry.componentType === componentType
+    })!;
+    return entry.input_types!;
   }
 }
