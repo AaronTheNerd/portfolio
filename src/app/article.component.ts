e0da780de9d3817f2b546820791288d99e6d3fa1
AaronTheNerd <bargeaaron@gmail.com>
2024-03-05 07:03:45 -0500

    Fix: Changed some formatting to avoid overlapping on small viewports

--- src/app/article.component.ts
+++ src/app/article.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-article',
   template: `
-  <div class="row" style="width: 100%">
+  <div class="row" style="width: 100%; margin-left: 0px">
     <div class="col-xs-12 col-sm-11 col-md-10 mx-auto px-0 py-5">
       <ng-content></ng-content>
     </div>
