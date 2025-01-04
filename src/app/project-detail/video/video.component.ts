7e65261438e05566a3d860cd0720e981a296c280
AaronTheNerd <bargeaaron@gmail.com>
2025-01-04 13:51:47 -0500

    Feature: Added loop input to the video component

--- src/app/project-detail/video/video.component.ts
+++ src/app/project-detail/video/video.component.ts
@@ -2,12 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-video',
-  templateUrl: './video.component.html',
-  styleUrl: './video.component.scss'
+  templateUrl: './video.component.html'
 })
 export class VideoComponent {
   @Input() video_url!: string;
   @Input() autoplay!: boolean;
   @Input() controls!: boolean;
   @Input() description!: string;
+  @Input() loop!: boolean;
 }
