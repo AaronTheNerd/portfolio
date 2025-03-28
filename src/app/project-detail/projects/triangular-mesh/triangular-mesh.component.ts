300986d9a64e8469b4e7c9315d7cc172063da8b6
AaronTheNerd <bargeaaron@gmail.com>
2025-03-28 06:24:39 -0400

    Feature: Added Triangular Mesh Animator examples

--- src/app/project-detail/projects/triangular-mesh/triangular-mesh.component.ts
+++ src/app/project-detail/projects/triangular-mesh/triangular-mesh.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-triangular-mesh',
-  templateUrl: './triangular-mesh.component.html',
-  styleUrl: './triangular-mesh.component.scss'
+  templateUrl: './triangular-mesh.component.html'
 })
 export class TriangularMeshComponent {
-
+  examples = ["assets/gif8.gif", "assets/gif17.gif"];
 }
